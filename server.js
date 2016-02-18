994c8b3413edb93a64a754f99f6adf0ac0e7d9e7
Glen Roberts <glensroberts@gmail.com>
2016-02-17 19:25:27 -0500

    A lot more to do, just uploading this as a stopping point. Need to figure out what's going on with the variables.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-request("http://archive.ubuntu.com/ubuntu/pool/", function (error, response, body) {
+request("http://archive.ubuntu.com/ubuntu/", function (error, response, body) {
     if (error) {
         console.log("Error: " + error);
     }
@@ -11,20 +11,31 @@ request("http://archive.ubuntu.com/ubuntu/pool/", function (error, response, bod
     var $ = cheerio.load(body);
     var table = $("table");
 
-    table.find('tr').each(function (i) {
-        var $tds = $(this).find('td'),
-            tableName = $tds.eq(1).text(),
-            tableLastModified = $tds.eq(2).text(),
-            tableSize = $tds.eq(3).text();
+    getRowInfo(table);
 
+    function recursive(tableName) {
+        if (tableName.slice(-1) == '/') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function getRowInfo(table) {
+        table.find('tr').each(function (i) {
+            var $tds = $(this).find('td');
+            tableName = $tds.eq(1).text();
+            tableLastModified = $tds.eq(2).text();
+            tableSize = $tds.eq(3).text();
+        printResults(tableName, tableLastModified, tableSize, recursive(tableName));
+        });
+    }
+
+    function printResults(tableName, tableLastModified, tableSize, directoryStatus) {
         if (tableName == '' | tableName == 'Parent Directory' | tableName == 'ubuntu/') {
 
         } else {
-            console.log('Name: ' + tableName
-                + '\nLast Modified: ' + tableLastModified
-                + '\nSize: ' + tableSize);
+            console.log('Name: ' + tableName + ' Directory: ' + directoryStatus + ' Last Modified: ' + tableLastModified + ' Size: ' + tableSize);
         }
-
-
-    });
+    }
 });
